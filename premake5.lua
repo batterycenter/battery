ffb1dd28e7417fef5b41c48d19112339e8d0af83
HerrNamenlos123 <herrnamenlos123@gmail.com>
2022-03-28 20:19:50 +0200

    First step towards conan

--- premake5.lua
+++ premake5.lua
@@ -6,27 +6,39 @@ function appendTable(tableA, tableB)
     end
 end
 
-function fileExists(name)
-    local f=io.open(name,"r")
-    if f~=nil then io.close(f) return true else return false end
-end
-
-
--- Check if OpenSSL is available
-if not fileExists("C:/Program Files/OpenSSL-Win64/include/openssl/err.h") then
-    term.pushColor(term.red)
-    printf("Warning: OpenSSL was not found! Disabling support for networking...")
-    BATTERYENGINE_DISABLE_NETWORKING=true
-    term.popColor()
-end
+newoption {
+    trigger = "ENABLE_NETWORKING",
+    value = "on",
+    description = "Enable or disable ASIO TCP/UDP networking",
+    allowed = {
+       "on",
+       "off"
+    },
+    default = "on"
+}
 
 
 
--- Dependencies (git submodules)
-include "modules/allegro5-binaries"
 
--- Main framework project
-project "BatteryEngine"
+-- Include the generated dependency paths from Conan
+include "conanbuildinfo.premake5.lua"
+conan_basic_setup()
+
+
+
+
+-- Define the solution
+workspace "Battery"
+    configurations { "Debug", "Release", "Deploy" }
+
+    platforms { "x64" }
+    defaultplatform "x64"
+    startproject "Battery"
+
+
+
+-- Defines the static library project
+project "Battery"
     kind "StaticLib"
     language "C++"
 	cppdialect "C++17"
@@ -34,13 +46,7 @@ project "BatteryEngine"
     location "build"
     targetdir "bin/%{cfg.buildcfg}"
     targetname "%{prj.name}"
-    
-    -- Platforms
-    filter { "platforms:x86" }
-        architecture "x86"
-
-    filter { "platforms:x64" }
-        architecture "x86_64"
+    architecture "x86_64"
     
     -- Per-configuration defines
     filter "configurations:Debug"
@@ -52,7 +58,7 @@ project "BatteryEngine"
     filter "configurations:Release"
         defines { "NDEBUG", "NDEPLOY" }
         runtime "Release"
-        symbols "Off"
+        symbols "On"
         optimize "On"
 
     filter "configurations:Deploy"
@@ -93,35 +99,11 @@ project "BatteryEngine"
     -- Main source files
     files ({ "include/**", "src/**" })
 
-    -- Serial library
-    files ({ "modules/serial/src/serial.cc" })
-    files ({ "modules/serial/src/impl/win.cc" })
-    files ({ "modules/serial/src/impl/list_ports/list_ports_win.cc" })
-
     -- Clip library
     files ({ "modules/clip/clip.cpp" })
     files ({ "modules/clip/clip_win.cpp" })
     files ({ "modules/clip/image.cpp" })
 
-    -- spdlog library
-    files ({ "modules/spdlog/src/async.cpp" })
-    files ({ "modules/spdlog/src/cfg.cpp" })
-    files ({ "modules/spdlog/src/color_sinks.cpp" })
-    files ({ "modules/spdlog/src/file_sinks.cpp" })
-    files ({ "modules/spdlog/src/fmt.cpp" })
-    files ({ "modules/spdlog/src/spdlog.cpp" })
-    files ({ "modules/spdlog/src/stdout_sinks.cpp" })
-
-    -- ImGui/ImPlot library
-    files ({ "modules/imgui/imgui.cpp" })
-    files ({ "modules/imgui/imgui_demo.cpp" })
-    files ({ "modules/imgui/imgui_draw.cpp" })
-    files ({ "modules/imgui/imgui_tables.cpp" })
-    files ({ "modules/imgui/imgui_widgets.cpp" })
-    files ({ "modules/imgui/backends/imgui_impl_allegro5.cpp" })
-    files ({ "modules/implot/implot.cpp" })
-    files ({ "modules/implot/implot_demo.cpp" })
-    files ({ "modules/implot/implot_items.cpp" })
 
 
 
@@ -149,15 +131,16 @@ project "BatteryEngine"
 
 
     -- Include and linker information for premake projects using this library
-    BATTERYENGINE_INCLUDE_DIRS = {}
-    appendTable(BATTERYENGINE_INCLUDE_DIRS, _includedirs)
-    appendTable(BATTERYENGINE_INCLUDE_DIRS, ALLEGRO5_INCLUDE_DIRS)
+    BATTERY_INCLUDE_DIRS = {}
+    appendTable(BATTERY_INCLUDE_DIRS, _includedirs)
+    appendTable(BATTERY_INCLUDE_DIRS, ALLEGRO5_INCLUDE_DIRS)
 
-    BATTERYENGINE_LINK_DIRS = { _SCRIPT_DIR .. "/bin/%{cfg.buildcfg}/", "C:/Program Files/OpenSSL-Win64/lib/VC/static/" }
-    appendTable(BATTERYENGINE_LINK_DIRS, ALLEGRO5_LINK_DIRS)
+    BATTERY_LINK_DIRS = { _SCRIPT_DIR .. "/bin/%{cfg.buildcfg}/", "C:/Program Files/OpenSSL-Win64/lib/VC/static/" }
+    appendTable(BATTERY_LINK_DIRS)
 
-    BATTERYENGINE_LINKS = { "BatteryEngine", "opengl32", "winmm", "shlwapi" }
-    appendTable(BATTERYENGINE_LINKS, ALLEGRO5_LINKS)
-    if not BATTERYENGINE_DISABLE_NETWORKING == true then
-        appendTable(BATTERYENGINE_LINKS, { "libcrypto64MTd", "libssl64MTd" } )
+    BATTERY_LINKS = { "Battery" }
+    BATTERY_SYSTEM_LINKS = { "opengl32", "winmm", "shlwapi" }
+    appendTable(BATTERY_LINKS, ALLEGRO5_LINKS)
+    if not BATTERY_DISABLE_NETWORKING == true then
+        appendTable(BATTERY_LINKS, { "libcrypto64MTd", "libssl64MTd" } )
     end
