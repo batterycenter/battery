13b61a35cb8cb6603bb3f20f2163b751dc2f23de
HerrNamenlos123 <herrnamenlos123@gmail.com>
2022-10-02 19:59:45 +0200

    Remove submodules

--- examples/BatteryApp/include/BatteryApp.h
+++ examples/BatteryApp/include/BatteryApp.h
@@ -5,6 +5,8 @@ class BatteryApp : public Battery::Application {
 public:
     BatteryApp() {}
 
+    std::unique_ptr<Battery::TrayIcon> tray;
+
     void OnStartup() override;
     //void OnUpdate() override;
     void OnRender() override;
--- examples/BatteryApp/src/BatteryApp.cpp
+++ examples/BatteryApp/src/BatteryApp.cpp
@@ -3,15 +3,26 @@
 #include "BatteryApp.h"
 #include "Battery/Extern/magic_enum.hpp"
 
+#include "windows.h"
+#include "shellapi.h"
+
 void BatteryApp::OnStartup() {
     using namespace Battery;
 
-    
+    auto im = Battery::DecodeBase64(BATTERY_DEFAULT_WINDOW_ICON_BASE64);
+    sf::Image image;
+    image.loadFromMemory(&im[0], im.size());
 
-    CloseApplication();
+    tray = std::make_unique<Battery::TrayIcon>(image, "Quick tooltip");
+
+    tray->attachLeftClickCallback([&] { LOG_DEBUG("Left click"); });
+    tray->attachRightClickCallback([&] { LOG_DEBUG("Right click"); });
 }
 
 void BatteryApp::OnRender() {
+
+    tray->update();
+
     ImGui::ShowDemoWindow();
     
 }
