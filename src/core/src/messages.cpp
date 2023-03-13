9f0882b05db9ce32bee7c8f9e2374818c1df59de
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-03-13 23:31:09 +0100

    Refactor messages.cpp

--- src/core/src/messages.cpp
+++ src/core/src/messages.cpp
@@ -29,41 +29,40 @@ namespace battery {
         LocalFree(messageBuffer);
         return buffer;
     }
+    static void win32_message_box(const std::string& message, UINT options) {
+        auto text = battery::string::utf8_to_osstring(message);
+        ::MessageBoxW(nullptr, std::bit_cast<const wchar_t*>(text.c_str()), L"Error", options);
+    }
+#else
+    static void linux_run_zenity(const std::vector<std::string>& commands) {
+        battery::process zenity;
+        zenity.options.executable = "zenity";
+        zenity.options.arguments = commands;
+        zenity.execute_sync();
+    }
 #endif  // BATTERY_ARCH_WINDOWS
 
     void message_box_info(const std::string& message) {
 #ifdef BATTERY_ARCH_WINDOWS
-        auto text = battery::string::utf8_to_osstring(message);
-        ::MessageBoxW(nullptr, std::bit_cast<const wchar_t*>(text.c_str()), L"Error", MB_ICONINFORMATION | MB_OK);
+        win32_message_box(message, MB_ICONINFORMATION | MB_OK);
 #else
-        battery::process zenity;
-        zenity.options.executable = "zenity";
-        zenity.options.arguments = { "--info", "--text=" + message };
-        zenity.execute_sync();
+        linux_run_zenity({ "--info", "--text=" + message });
 #endif
     }
 
     void message_box_warning(const std::string& message) {
 #ifdef BATTERY_ARCH_WINDOWS
-        auto text = battery::string::utf8_to_osstring(message);
-        ::MessageBoxW(nullptr, std::bit_cast<const wchar_t*>(text.c_str()), L"Error", MB_ICONWARNING | MB_OK);
+        win32_message_box(message, MB_ICONWARNING | MB_OK);
 #else
-        battery::process zenity;
-        zenity.options.executable = "zenity";
-        zenity.options.arguments = { "--warning", "--text=" + message };
-        zenity.execute_sync();
+        linux_run_zenity({ "--warning", "--text=" + message });
 #endif
     }
 
     void message_box_error(const std::string& message) {
 #ifdef BATTERY_ARCH_WINDOWS
-        auto text = battery::string::utf8_to_osstring(message);
-        ::MessageBoxW(nullptr, std::bit_cast<const wchar_t*>(text.c_str()), L"Error", MB_ICONERROR | MB_OK);
+        win32_message_box(message, MB_ICONERROR | MB_OK);
 #else
-        battery::process zenity;
-        zenity.options.executable = "zenity";
-        zenity.options.arguments = { "--error", "--text=" + message };
-        zenity.execute_sync();
+        linux_run_zenity({ "--error", "--text=" + message });
 #endif
     }
 
