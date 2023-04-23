0715dd9fef6f7ad1e1ab552aa5d905e8577ae0e8
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-04-23 16:49:39 +0200

    Implement Lockfile examples

---
+++ examples/LockfileBlocking/src/main.cpp
@@ -0,0 +1,25 @@
+
+#include <battery/core/main.hpp>
+#include <battery/core/log.hpp>
+#include <battery/core/lockfile.hpp>
+#include <battery/core/time.hpp>
+
+int b::main([[maybe_unused]] const std::vector<std::string>& args) {
+
+    b::log::info("Running Example Lockfile. To test it, start this example multiple times. Have fun!");
+
+    b::log::debug("Waiting for 'lockfile.lock' to be locked");
+    b::scoped_lockfile lock("lockfile.lock");
+
+    b::log::debug("Locked 'lockfile.lock' successfully. We have exclusive access now!");
+    for (int i = 0; i < 10; i++) {
+        b::print("Performing critical operations in a folder...\n");
+        b::sleep(1);
+    }
+    b::log::debug("Critical operations finished. Destructor unlocks the lockfile and allows another process to lock it.");
+
+    b::print("\n");
+    b::log::info("Example Lockfile finished. Please take a look at how it is implemented. Happy coding!");
+
+    return 0;
+}
--- examples/LockfileNonBlocking/src/main.cpp
+++ examples/LockfileNonBlocking/src/main.cpp
@@ -8,13 +8,13 @@ int b::main([[maybe_unused]] const std::vector<std::string>& args) {
 
     b::log::info("Running Example Lockfile. To test it, start this example multiple times. Have fun!");
 
-    b::log::debug("Locking 'lockfile.lock'");
+    b::log::debug("Trying to lock 'lockfile.lock'");
     try {
-        b::scoped_lockfile lock("lockfile.lock");
+        b::scoped_lockfile lock("lockfile.lock", false);
 
         b::log::debug("Locked 'lockfile.lock' successfully. We have exclusive access now!");
-        for (int i = 0; i < 5; i++) {
-            b::print("Performing critical operations in a folder...");
+        for (int i = 0; i < 10; i++) {
+            b::print("Performing critical operations in a folder...\n");
             b::sleep(1);
         }
         b::log::debug("Critical operations finished. Destructor unlocks the lockfile and allows another process to lock it.");
