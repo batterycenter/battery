59a1981fb6f860a1d444fa8bde823624374afcae
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-06-01 17:52:36 +0200

    Rewrite b::window::create_window()

--- src/modules/graphics/include/battery/graphics/application.hpp
+++ src/modules/graphics/include/battery/graphics/application.hpp
@@ -18,8 +18,8 @@ namespace b {
         virtual void cleanup() = 0;
 
         template<typename T>
-        [[nodiscard]] std::shared_ptr<T> create_window() {
-            auto window_ptr = std::make_shared<T>();
+        void create_window(std::shared_ptr<T>& window_ptr) {
+            window_ptr = std::make_shared<T>();
             m_windows.push_back(window_ptr);
             window_ptr->setup();
             return window_ptr;
