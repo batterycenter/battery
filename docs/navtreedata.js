5392a0cdb2ad4d88f50b37b629b3df6ca1574d6d
batterycenter <herrnamenlos123@gmail.com>
2023-08-24 20:04:33 +0000

    Update from https://github.com/batterycenter/battery/commit/390d89642eccb03e94e848fa3879f4ca1c912833

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -26,37 +26,36 @@ var NAVTREE =
 [
   [ "Battery", "index.html", [
     [ "Battery Library", "index.html", [
-      [ "Heading", "index.html#autotoc_md17", null ],
-      [ "Code fragment", "index.html#autotoc_md18", null ],
-      [ "HLKSAs", "index.html#autotoc_md19", null ],
-      [ "ds", "index.html#autotoc_md20", null ],
-      [ "fd", "index.html#autotoc_md21", null ],
-      [ "Doxygen", "index.html#autotoc_md22", [
-        [ "Doxygen sub-group", "index.html#autotoc_md23", null ]
+      [ "Heading", "index.html#autotoc_md16", null ],
+      [ "Code fragment", "index.html#autotoc_md17", null ],
+      [ "HLKSAs", "index.html#autotoc_md18", null ],
+      [ "ds", "index.html#autotoc_md19", null ],
+      [ "fd", "index.html#autotoc_md20", null ],
+      [ "Doxygen", "index.html#autotoc_md21", [
+        [ "Doxygen sub-group", "index.html#autotoc_md22", null ]
       ] ]
     ] ],
     [ "Battery Guidelines", "guidelines.html", [
-      [ "UTF-8 Everywhere", "guidelines.html#autotoc_md0", null ],
-      [ "Working with Strings", "guidelines.html#autotoc_md1", null ],
-      [ "File I/O", "guidelines.html#autotoc_md2", [
-        [ "Unicode (UTF-8)", "guidelines.html#autotoc_md3", [
-          [ "What is Unicode?", "guidelines.html#autotoc_md4", null ],
-          [ "The Problem", "guidelines.html#autotoc_md5", null ]
-        ] ],
-        [ "Terminology", "guidelines.html#autotoc_md6", null ],
-        [ "Working with Unicode", "guidelines.html#autotoc_md7", null ],
-        [ "Source files", "guidelines.html#autotoc_md8", null ],
+      [ "UTF-8 Everywhere", "guidelines.html#autotoc_md0", [
+        [ "Unicode Terminology", "guidelines.html#autotoc_md1", null ]
+      ] ],
+      [ "Programming Environment", "guidelines.html#autotoc_md2", null ],
+      [ "Working with Strings", "guidelines.html#autotoc_md3", null ],
+      [ "File I/O", "guidelines.html#autotoc_md4", null ],
+      [ "Working with the Windows API", "guidelines.html#autotoc_md5", [
+        [ "Working with Unicode", "guidelines.html#autotoc_md6", null ],
+        [ "Source files", "guidelines.html#autotoc_md7", null ],
         [ "WinAPI / Linux API", "guidelines.html#WIN_LINUX_API", [
-          [ "WinAPI: Do not use ANSI functions!", "guidelines.html#autotoc_md9", null ],
+          [ "WinAPI: Do not use ANSI functions!", "guidelines.html#autotoc_md8", null ],
           [ "Use OsString whenever applicable", "guidelines.html#OsString", null ],
-          [ "Using high-level containers", "guidelines.html#autotoc_md10", null ],
-          [ "Keeping WinAPI / Linux API cross-platform", "guidelines.html#autotoc_md11", null ]
+          [ "Using high-level containers", "guidelines.html#autotoc_md9", null ],
+          [ "Keeping WinAPI / Linux API cross-platform", "guidelines.html#autotoc_md10", null ]
         ] ]
       ] ],
-      [ "Lockfiles", "guidelines.html#autotoc_md12", [
-        [ "Different types", "guidelines.html#autotoc_md13", null ]
+      [ "Lockfiles", "guidelines.html#autotoc_md11", [
+        [ "Different types", "guidelines.html#autotoc_md12", null ]
       ] ],
-      [ "OsString -> Always terminates at '\\0'", "guidelines.html#autotoc_md14", null ]
+      [ "OsString -> Always terminates at '\\0'", "guidelines.html#autotoc_md13", null ]
     ] ],
     [ "Getting Started", "md_docs_getting_started.html", null ],
     [ "Todo List", "todo.html", null ],
@@ -82,11 +81,11 @@ var NAVTREE =
 
 var NAVTREEINDEX =
 [
-"",
-"classb_1_1tray_1_1separator.html#a5332b6dc78f8de76dd4c6fa37b9f560d",
-"fs_8hpp.html#aeeaa34d45b8c21d377007bc39558ee45",
-"group__Time.html",
-"structfmt_1_1formatter_3_01b_1_1fs_1_1path_01_4.html#ac6467828ce3d8279e6e7be757d6fd407"
+"annotated.html",
+"classb_1_1tray_1_1synced__toggle.html#a400e617f0d1948aff9089de47185df21",
+"group__Lockfile.html#ga238f952b89fde5c57c7cc40c26ea50f9",
+"group__Time.html#gae1f8548eef8a61e8c814d3cfed032c30",
+"traybase_8hpp.html#a62d86b20bfc7ff4d6b35a355a7366415"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
