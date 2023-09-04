827ace3c4f66c4c1b0b6627b905189e098dd37e9
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-09-04 11:11:47 +0200

    Add CLI11 b::fs::path operators

--- src/core/include/battery/core/fs.hpp
+++ src/core/include/battery/core/fs.hpp
@@ -1286,4 +1286,20 @@ template <> struct fmt::formatter<b::fs::path> {
     }
 };
 
+// Specify stream operators to be able to use `b::fs::path` with CLI11
+namespace CLI {
+    std::istringstream &operator>>(std::istringstream &in, b::fs::path &val) {
+        std::string str;
+        in >> str;
+        val = b::fs::path(str);
+        return in;
+    }
+
+    template <typename T, typename R>
+    std::stringstream &operator<<(std::stringstream &in, b::fs::path &val) {
+        in << val.string();
+        return in;
+    }
+} // namespace CLI
+
 #endif // BATTERY_CORE_FS_HPP
