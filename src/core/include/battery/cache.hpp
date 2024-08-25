b6e907b00c7e17b92c45ee9d1753ab3b1b511897
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-08-25 17:40:37 +0200

    add cache file

--- src/core/include/battery/cache.hpp
+++ src/core/include/battery/cache.hpp
@@ -1,75 +1,82 @@
 #pragma once
 
-#include <string>
-#include <utility>
+#include "battery/extern/json.hpp"
 #include "fs.hpp"
 #include "log.hpp"
-#include "glaze/glaze.hpp"
+#include <string>
+#include <utility>
 
 namespace b {
 
-    // This class will read a file, or create it if it does not exist, but never delete it.
-    // It stores key-value pairs in the form of a json file.
-//    class cachefile {
-//    public:
-//        cachefile();
-//        cachefile(const b::fs::path& filename);
-//
-//        class entry {
-//        public:
-//            entry(cachefile& parent, b::string key) : parent(parent), key(std::move(key)) {}
-//
-//            operator nlohmann::json() { return parent.get<nlohmann::json>(key); }
-//            operator b::string() { return parent.get<std::string>(key); }
-//
-//            template<typename T>
-//            operator T() {
-//                return parent.get<T>(key);
-//            }
-//
-//            template<typename T>
-//            entry& operator=(T j) {
-//                parent.set<T>(key, j);
-//                return *this;
-//            }
-//
-//        private:
-//            cachefile& parent;
-//            b::string key;
-//        };
-//
-//        entry operator[](const b::string& key) {
-//            return entry(*this, key);
-//        }
-//
-//        template<typename T>
-//        T get(const b::string& key) {
-//            reload();
-//            return json[key.str()];
-//        }
-//
-//        template<typename T>
-//        void set(const b::string& key, T value) {
-//            reload();
-//            json[key] = value;
-//
-//            b::fs::write_text_file(filename, json.dump(4));
-//        }
-//
-//        bool contains(const b::string& key) {
-//            reload();
-//            return json.contains(key);
-//        }
-//
-//    private:
-//        void reload() {
-//            auto file = b::fs::read_text_file(filename);
-//            json = nlohmann::json::parse(file);
-//        }
-//
-//    private:
-//        b::fs::path filename;
-//        nlohmann::json json;
-//    };
+// This class will read a file, or create it if it does not exist, but never delete it.
+// It stores key-value pairs in the form of a json file.
+class cachefile {
+public:
+    cachefile();
+    cachefile(const b::fs::path& filename);
 
-}
+    class entry {
+    public:
+        entry(cachefile& parent, std::string key)
+            : parent(parent)
+            , key(std::move(key))
+        {
+        }
+
+        operator nlohmann::json()
+        {
+            return parent.get<nlohmann::json>(key);
+        }
+
+        template <typename T> operator T()
+        {
+            return parent.get<T>(key);
+        }
+
+        template <typename T> entry& operator=(T j)
+        {
+            parent.set<T>(key, j);
+            return *this;
+        }
+
+    private:
+        cachefile& parent;
+        std::string key;
+    };
+
+    entry operator[](const std::string& key)
+    {
+        return entry(*this, key);
+    }
+
+    template <typename T> T get(const std::string& key)
+    {
+        auto json = load();
+        return json[key];
+    }
+
+    template <typename T> void set(const std::string& key, T value)
+    {
+        auto json = load();
+        json[key] = value;
+        b::fs::write(filename, json.dump(4));
+    }
+
+    bool contains(const std::string& key)
+    {
+        auto json = load();
+        return json.contains(key);
+    }
+
+private:
+    nlohmann::json load()
+    {
+        auto file = b::fs::read(filename);
+        return nlohmann::json::parse(file);
+    }
+
+private:
+    b::fs::path filename;
+};
+
+} // namespace b
--- src/core/src/cache.cpp
+++ src/core/src/cache.cpp
@@ -1,22 +1,26 @@
 
 #include "battery/cache.hpp"
 #include "battery/fs.hpp"
+#include "battery/folders.hpp"
 
 namespace b {
 
-//    cachefile::cachefile() {
-//
-//    }
-//
-//    cachefile::cachefile(const b::fs::path& filename) : filename(filename) {
-//        if (!b::fs::is_regular_file(filename)) {
-//            b::fs::ofstream file(filename);
-//            if (file.fail()) {
-//                throw std::runtime_error(b::format("b::cachefile: Failed to open file '{}' for writing: ", filename, strerror(errno)));
-//            }
-//            file << nlohmann::json::parse("{}").dump(4);
-//            file.close();
-//        }
-//    }
+static void init(const b::fs::path& filename)
+{
+    if (!b::fs::exists(filename)) {
+        b::fs::write(filename, nlohmann::json::parse("{}").dump(4));
+    }
+}
+
+static b::fs::path defaultFilename() { b::Folders::AppCacheDir() / "app.cache"; }
+
+cachefile::cachefile()
+    : filename(defaultFilename()) { init(filename) }
+
+    cachefile::cachefile(const b::fs::path& filename)
+    : filename(filename)
+{
+    init(filename)
+}
 
 } // namespace b
